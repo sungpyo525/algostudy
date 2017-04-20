dec0646651c877a4750a534b90ccc5665d28247e
ruby <sungpyo525@gmail.com>
2017-04-20 14:32:15 +0900

    숫자읽기 수정

--- sungpyo525/hanwha170417/ReadingNumber.java
+++ sungpyo525/hanwha170417/ReadingNumber.java
@@ -1,5 +1,6 @@
 package com.hanwha170417;
 
+import java.io.InputStreamReader;
 import java.util.Scanner;
 // 숫자 읽기
 public class ReadingNumber {
@@ -13,24 +14,34 @@ public class ReadingNumber {
 		
 		for(int i=0; i<input.length(); i++){
 			int index=input.charAt(i)-48; // 0의 아스키코드가 48이라서 - 해줌
+			
 			// 만 자리 이전
 			if(i<input.length()-5){
 				if(index!=0 && (index!=1 || input.length()==9)) // 억 단위 까지 가면 숫자가 1이어도 출력해줘야 햠
 					System.out.print(num[index]);
 				if(index!=0)
 					System.out.print(danwi[8-input.length()+1+i]);
-			} // 만의 자리 (만은 5자리 이상의 숫자부터는 무조건 써줘야한다.)
+			}
+			
+			
+			// 만의 자리 (만은 5자리 이상의 숫자부터는 무조건 써줘야한다.)
 			else if(i==input.length()-5){
-				if(index!=0 && (input.length()>6 || index!=1)) //  (input.length()>6 || index!=1) 이유 : 5자리 숫자가 1로 시작하는 경우를 위해
+				if(index!=0 && (input.length()>=6 || index!=1)) //  (input.length()>=6 || index!=1) 이유 : 5자리 숫자가 1로 시작하는 경우를 위해
 					System.out.print(num[index]);
 				System.out.print(danwi[4]);
-			} // 만의 자리 이후
+			}
+			
+			
+			// 만의 자리 이후
 			else if(i>input.length()-5 && i!=input.length()-1){
 				if(index!=0 && index!=1)
 					System.out.print(num[index]);
 				if(index!=0)
 					System.out.print(danwi[8-input.length()+1+i]);
-			} // 맨 마지막 자리 수
+			}
+			
+			
+			// 맨 마지막 자리 수
 			else if(i==input.length()-1){
 				if(input.charAt(i)!='0')
 					System.out.print(num[index]);
